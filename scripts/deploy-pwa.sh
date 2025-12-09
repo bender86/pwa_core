773a7bc04befee92d3277f25aa96ff8ee2a6d5a7
bender86 <maesfab@gmail.com>
2025-12-09 22:27:55 +0100

    add gitignore + deploy-pwa

---
+++ scripts/deploy-pwa.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Couleurs
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+RED='\033[0;31m'
+NC='\033[0m'
+
+echo -e "${BLUE}?? D�marrage du d�ploiement de la PWA...${NC}\n"
+
+# Se d�placer dans le r�pertoire du projet
+cd /home/admin_maef/Projects/PWA/pwa_core/src/PWA.Auth
+
+# Build de la PWA Blazor WebAssembly
+echo -e "${BLUE}?? Build de la PWA...${NC}"
+dotnet publish -c Release -o out
+
+# Verifier si le build a r�ussi
+if [ $? -ne 0 ]; then
+    echo -e "${RED}? Erreur lors du build${NC}"
+    exit 1
+fi
+
+# Copie des fichiers vers le site
+echo -e "${BLUE}?? Copie des fichiers...${NC}"
+sudo rsync -av -av --exclude='.env'  /home/admin_maef/Projects/PWA/pwa_core/src/PWA.Auth/out/wwwroot/ /var/www/PWA_Auth/
+
+
+echo -e "\n${GREEN}? D�ploiement r�ussi ! La PWA est disponible.${NC}"
--- src/PWA.Auth/Program.cs
+++ src/PWA.Auth/Program.cs
@@ -6,11 +6,11 @@ using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Authorization;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<App>("#app"); 
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 // Configuration de l'API - Récupérer depuis appsettings.json
-var apiBaseUrl = "http://localhost:5035/api/v1/";// builder.Configuration["ApiBaseUrl"] ?? "https://maeshome.ddns.net/api/v1/";
+var apiBaseUrl = builder.Configuration["ApiBaseUrl"] ?? "https://maeshome.ddns.net/api/v1/";
 
 // HttpClient configuré avec l'URL de base de l'API
 builder.Services.AddScoped(sp => new HttpClient 
